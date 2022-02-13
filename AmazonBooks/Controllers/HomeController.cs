b05245235a206058bd634262171be8f0b2ebf30c
jessi472 <79349080+jessi472@users.noreply.github.com>
2022-02-12 20:13:37 -0700

    Add project files.

---
+++ AmazonBooks/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AmazonBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using AmazonBooks.Models.ViewModels;
+
+namespace AmazonBooks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAmazonBooksRepository repo;
+        public HomeController (IAmazonBooksRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var books = new BookViewModel
+            {
+              Books = repo.Books
+            .OrderBy(b => b.Title)
+            .Skip((pageNum-1 )* pageSize)
+            .Take(pageSize),
+
+            PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+                
+            return View(books);
+        }
+    }
+}
---
+++ AmazonBooks/Models/EFAmazonBooksRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonBooks.Models
+{
+    public class EFAmazonBooksRepository : IAmazonBooksRepository
+    {
+
+        private BookstoreContext context { get; set; }
+        public EFAmazonBooksRepository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
